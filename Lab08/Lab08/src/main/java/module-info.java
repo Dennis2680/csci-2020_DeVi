17498ba751f67038d040ad82b8da8febe93a626f
Dennis2680 <90404575+Dennis2680@users.noreply.github.com>
2022-03-31 10:40:50 -0400

    Delete Lab08/Lab08 directory

--- Lab08/Lab08/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.lab08 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires opencsv;
-
-
-    opens com.example.lab08 to javafx.fxml;
-    exports com.example.lab08;
-}
